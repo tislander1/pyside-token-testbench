6cad9e6d685055df19e9025b51a216d8dabc37b1
Tislander1 <blurryprojection@gmail.com>
2024-09-08 11:11:52 -0600

    add table

--- basic_pyside_gui2.py
+++ basic_pyside_gui2.py
@@ -1,6 +1,9 @@
 from PySide6.QtWidgets import *
 import sys, json, os
 
+from PySide6.QtCore import QAbstractTableModel, Qt
+import pandas as pd
+
 file_output = 'test.json'
 
 class Tokens():
@@ -12,8 +15,50 @@ class Tokens():
             'tree': {'type': 'combobox', 'label': 'Tree', 'options' : ['Pine', 'Maple', 'Palm'], 'datatype': 'str', 'default': 'Basket weaving', 'group': 'Cool'},
             'sleeping': {'type': 'checkbox', 'label': 'Sleeping', 'datatype': 'bool', 'default': False, 'group': 'Lame'},
             'story': {'type': 'plaintextedit', 'label': 'Story', 'datatype': 'str', 'default': 'It was a dark and stormy night.', 'group':  'Amazing'},
+            'table1': {'type': 'table', 'label': 'Roses', 'datatype': 'tabular', 'data': [['Pink', 20, 'B+'], ['Red', 15, 'A-']], 'columns': ['Color', 'Thorn count', 'Fanciness'], 'optional_row_labels': [], 'row_count': 10, 'group': 'Lame'},
+            'table2': {'type': 'table', 'label': 'AIs', 'datatype': 'tabular', 'data': [['Samsung', 5], ['Apple', 2]], 'columns': ['Maker', 'IQ'], 'optional_row_labels': ['Bixby', 'Siri'], 'row_count': 2, 'group': 'Cool'}
 			}
         self.config = {}
+
+class TableModel(QAbstractTableModel):
+    def __init__(self, data_table, row_count, column_headings, row_headings = []):
+        super(TableModel, self).__init__()
+        self.data_table = data_table
+        num_cols = len(column_headings)
+        while len(self.data_table) < row_count:
+            self.data_table.append(['' for ix in range(num_cols)])
+        self._row_count = row_count
+        self._column_headings = column_headings
+        self._row_headings = row_headings
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self.data_table[index.row()][index.column()]
+    def rowCount(self, index):
+        # The length of the outer list.
+        return self._row_count
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self.data_table[0])
+    def headerData(self, section, orientation, role):
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._column_headings[section])
+            if orientation == Qt.Vertical:
+                if self._row_headings and len(self._row_headings) == self._row_count:
+                    return str(self._row_headings[section])
+                else:
+                    return str(section)
+    def flags(self, index):
+        return Qt.ItemIsSelectable|Qt.ItemIsEnabled|Qt.ItemIsEditable
+    def setData(self, index, value, role):
+        if role == Qt.EditRole:
+            self.data_table[index.row()][index.column()] = value
+            return True
 
 class Window(QDialog):
 	# constructor
@@ -21,7 +66,7 @@ class Window(QDialog):
         super(Window, self).__init__()
         self.setWindowTitle("PySide Token Test 1")
         token_groups = ['Lame', 'Cool', 'Amazing']
-        self.setGeometry(100, 100, 300, 400)
+        self.setGeometry(100, 100, 700, 500)
         self.layout = QFormLayout()
 
         self.tok = Tokens()
@@ -62,6 +107,8 @@ class Window(QDialog):
                     gui_component.setChecked(bool(val))
                 elif self.tok.tokens[key]['type'] in ['plaintextedit']:
                     gui_component.setPlainText(val)
+                elif self.tok.tokens[key]['type'] in ['table']:
+                    gui_component.model().data_table = val
 
 		
     def draw_group_box(self, group_name):
@@ -84,7 +131,12 @@ class Window(QDialog):
                     obj = getattr(self, key)
                 elif value['type'] == 'plaintextedit':
                     setattr(self, key, QPlainTextEdit())
-                    obj = getattr(self, key)                   
+                    obj = getattr(self, key)
+                elif value['type'] == 'table':
+                    setattr(self, key, QTableView())
+                    obj = getattr(self, key)
+                    obj.setModel(TableModel(data_table = value['data'], row_count = value['row_count'], column_headings = value['columns'], row_headings = value['optional_row_labels']))
+                              
                 self.formGroupBoxes[group_name][1].addRow(QLabel(value['label']), obj)  #add row to layout
         self.formGroupBoxes[group_name][0].setLayout(self.formGroupBoxes[group_name][1])
 
@@ -102,6 +154,8 @@ class Window(QDialog):
                 result = getattr(self, key).isChecked()
             elif val['type'] in ['plaintextedit']:
                 result = getattr(self, key).toPlainText()
+            elif val['type'] in ['table']:
+                result = getattr(self, key).model().data_table
             else:
                 result = 'ERROR, UNKNOWN TYPE!!'
             print(val['label'] +': ' + str(result))
